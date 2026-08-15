1940da47a8bca1c92bbf017218cdfe8194443e9c
gakupoi <ram.alejandro71@gmail.com>
2026-08-15 17:41:09 -0600

    added function to enumerate tokens used per response

--- main.py
+++ main.py
@@ -15,19 +15,28 @@ def main():
         base_url="https://openrouter.ai/api/v1",
         api_key=api_key,
     )
+    
+    message_user = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    print(f"User prompt: {message_user}")
+
 
     response = client.chat.completions.create(
         model="openrouter/free",
         messages=[
             {
                 "role": "user",
-                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+                "content": message_user,
             }
 
         ],
     )
-    
-    print("Response")
+
+    if not response.usage:
+        raise RuntimeError("Usage is not actively")
+
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
+    print("Response:")
     print(response.choices[0].message.content)
 
 if __name__ == "__main__":
