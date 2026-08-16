6b1fdde1a1f6e61bcf7b374572ae06e9ba30a7b8
gakupoi <ram.alejandro71@gmail.com>
2026-08-15 21:49:26 -0600

    supported get file function but not already integrated to our agent

---
+++ functions/get_files_info.py
@@ -0,0 +1,28 @@
+import os
+
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+    try:
+        working_dir_abs = os.path.abspath (working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+
+        # Will be True or False
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        # return f'Success: "{directory}" is within the working directory'
+
+    except Exception as e:
+        return f"Error: {e}"
+
+    result = ""
+    list_files = os.listdir(target_dir)
+    for file in list_files:
+        file_path = os.path.join(target_dir,file)
+        size = os.path.getsize(file_path)
+        is_dir = os.path.isdir(file_path)
+        result += (f"- {file}: file_size={size}, is_dir={is_dir}\n")
+    
+    return result
