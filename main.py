40a614a01c6c56c577a0b87a1d412aee863c01aa
gakupoi <ram.alejandro71@gmail.com>
2026-08-15 18:24:18 -0600

    added flag notes to added information required by user

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from openai import OpenAI
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    
+
     load_dotenv()
     api_key = os.environ.get("OPENROUTER_API_KEY")
-    
     if not api_key:
          raise RuntimeError(f"api_key is not founded")
     
@@ -16,12 +21,6 @@ def main():
         base_url="https://openrouter.ai/api/v1",
         api_key=api_key,
     )
-    
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    args = parser.parse_args()
-
-    print(f"User prompt: {args.user_prompt}")
 
     messages = [
         {"role": "user", "content": args.user_prompt},
@@ -35,8 +34,12 @@ def main():
     if not response.usage:
         raise RuntimeError("Usage is not actively")
 
-    print(f"Prompt tokens: {response.usage.prompt_tokens}")
-    print(f"Response tokens: {response.usage.completion_tokens}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+
+
     print("Response:")
     print(response.choices[0].message.content)
 
