c60bfee21f4a7a39fc6703517a7f308484988c34
gakupoi <ram.alejandro71@gmail.com>
2026-08-16 23:13:14 -0600

    added functions get file content and info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,10 +8,10 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         # Will be True or False
         valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
         if not valid_target_dir:
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            return f'Cannot list "{directory}" as it is outside the permitted working directory'
 
         if not os.path.isdir(target_dir):
-            return f'Error: "{directory}" is not a directory'
+            return f'"{directory}" is not a directory'
         # return f'Success: "{directory}" is within the working directory'
 
     except Exception as e:
